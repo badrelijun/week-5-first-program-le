e7e43495cc4f57581a163926a5fdc8f460acde70
badrelijun <lorenzoescobar741@gmail.com>
2026-02-18 12:04:23 -0600

    Authored by: Enzo Co-authored-by: uhVlento <uhVlento@users.noreply.github.com>

---
+++ partner2_02-18.py
@@ -0,0 +1,51 @@
+# 1. Print a message to the screen
+print("Hello World")
+
+# 2. Change the message to your own words and run again
+print("Hi guys")
+
+
+# 3. Print your favorite food
+favFood="Chickfila"
+print("I want",favFood)
+
+# 4. Try printing a number instead of text
+num=15
+print(num)
+
+# 5. Create a variable that stores your name
+name="Lorenzo"
+
+# 6. Print the variable
+print("My name is",name)
+
+# 7. Change the name inside the variable and run again
+name="enzo"
+print("My nickname is",name)
+
+# 8. Create a variable with a number
+age = 18
+
+# 9. Print the age
+print(age)
+
+# 10. Add 2 to the age and print the result
+print(2 + age)
+
+# 11. Ask the user for their name
+user_name = input("What is your name? ")
+
+# 12. Print a greeting using their name
+print(f"Welcome, {user-name}!")
+
+# 13. Ask the user for a number
+user_num = input("Enter a number")
+
+# 14. Print the number they typed
+print(user_num)
+
+# 15. Print a goodbye message
+print(f"Goodbye, {user_name}!")
+
+#Resources used:
+#Stack Overflow
