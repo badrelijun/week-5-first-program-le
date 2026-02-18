43a416b2e808d238d8a7f9eb863f1170dd6210cc
badrelijun <lorenzoescobar741@gmail.com>
2026-02-18 11:38:07 -0600

    commit msg

---
+++ in_class.py
@@ -0,0 +1,31 @@
+# Example 1: Create a variable and assign a value
+name="Lorenzo"
+
+# Example 2: Create a numerical value and assign a value
+num=5
+
+# Example 3: Print text on the screen
+print('HELLO THERE, im printing text')
+
+# Example 4: Print what the variable name has inside
+print(name)
+
+# Example 5: Print a sentence using the variable 'name'
+print("my name is", name)
+
+# Example 6: Change the value of the variable and print it again
+name="Enzo"
+print("my nickname is", name)
+
+# Example 7: Ask the user what their favorite animal is
+print("Whats your favorite animal?")
+animal=input()
+print("I didnt know that", animal, "was your favorite animal")
+
+# Example 8: Use the variable created in ex2
+# if the variable is greater than 10 print a message
+# otherwise print another message
+if num>10:
+    print("Congrats Your number is greater than 10!")
+else:
+    print("Sorry your number is less than 10")
